ac99ecc45d5b1d40f05f1614098ad81a7f770d1a
CaioDetore <caio.detore@sou.unifeob.edu.br>
2025-05-08 20:15:13 -0300

    feat: caso de uso: Editar pergunta

---
+++ src/domain/forum/application/use-cases/edit-question.spec.ts
@@ -0,0 +1,50 @@
+import { expect, it, describe, beforeEach } from 'vitest'
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-question-repository'
+import { makeQuestion } from 'test/factories/make-question'
+import { UniqueEntityID } from 'src/core/entities/unique-entity-id'
+import { EditQuestionUseCase } from './edit-question'
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let sut: EditQuestionUseCase
+
+describe('Edit Question', () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    sut = new EditQuestionUseCase(inMemoryQuestionsRepository)
+  })
+
+  it('should be able to delete a question', async () => {
+    const newQuestion = makeQuestion({
+      authorId: new UniqueEntityID('author-1'),
+    }, new UniqueEntityID('question-1'))
+
+    await inMemoryQuestionsRepository.create(newQuestion)
+    
+    await sut.execute({
+      authorId: 'author-1',
+      title: 'Pergunta teste',
+      content: 'Conteudo teste',
+      questionId: newQuestion.id.toString()
+    })
+
+    expect(inMemoryQuestionsRepository.items[0]).toMatchObject({
+      title: 'Pergunta teste',
+      content: 'Conteudo teste',
+    })
+  })
+
+  it('should not be able to delete a question from another author', async () => {
+    const newQuestion = makeQuestion({
+      authorId: new UniqueEntityID('author-1'),
+    }, new UniqueEntityID('question-1'))
+
+    await inMemoryQuestionsRepository.create(newQuestion)
+    
+    expect(async () => await sut.execute({
+      authorId: 'author-2',
+      title: 'Pergunta teste',
+      content: 'Conteudo teste',
+      questionId: newQuestion.id.toString()
+    })).rejects.toBeInstanceOf(Error)
+  })
+})
---
+++ src/domain/forum/application/use-cases/edit-question.ts
@@ -0,0 +1,41 @@
+import { QuestionsRepository } from "../repositories/questions-repository";
+
+interface EditQuestionUseCaseRequest {
+  authorId: string
+  questionId: string
+  title: string
+  content: string
+}
+
+interface EditQuestionUseCaseResponse {
+}
+
+export class EditQuestionUseCase {
+  constructor(private questionRepository: QuestionsRepository) { }
+
+  async execute({
+    questionId,
+    authorId,
+    content,
+    title
+  }: EditQuestionUseCaseRequest): Promise<EditQuestionUseCaseResponse> {
+    const question = await this.questionRepository.findByID(questionId)
+
+    if (!question) {
+      throw new Error('Question not found.')
+    }
+
+    if (authorId !== question.authorId.toString()) {
+      throw new Error('User not allowed')
+    }
+
+    question.title = title
+    question.content = content
+
+    await this.questionRepository.save(question)
+
+    return {
+      question
+    }
+  }
+}
