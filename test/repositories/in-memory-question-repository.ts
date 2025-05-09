914bcf410cd39650c5bc9065cc2aaeee1fa86a4f
CaioDetore <caio.detore@sou.unifeob.edu.br>
2025-05-08 21:04:52 -0300

    feat: caso de uso listar perguntas recentes

--- test/repositories/in-memory-question-repository.ts
+++ test/repositories/in-memory-question-repository.ts
@@ -1,8 +1,17 @@
+import { PaginationParams } from "src/core/repositories/pagination-params";
 import { QuestionsRepository } from "src/domain/forum/application/repositories/questions-repository";
 import { Question } from "src/domain/forum/enterprise/entities/question";
 
 export class InMemoryQuestionsRepository implements QuestionsRepository {
   public items: Question[] = []
+
+  async findManyRecent({ page }: PaginationParams): Promise<Question[]> {
+    const question = this.items
+      .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
+      .slice((page - 1) * 20, page * 20)
+
+    return question
+  }
 
   async findByID(id: string): Promise<Question | null> {
     const question = this.items.find((item) => item.id.toString() === id)
